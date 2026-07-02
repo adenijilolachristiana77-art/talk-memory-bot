bce087526973d4da6ab89b90c3bf3821349b9645
adenijilolachristiana77-art <adenijilolachristiana77@gmail.com>
2026-07-02 02:03:15 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'voicebot-v1';
+const URLS_TO_CACHE = ['.','index.html','manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
